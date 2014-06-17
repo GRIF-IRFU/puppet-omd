5458e19dc3edbf1fb1242c5aa993dda19b788b80
Aaron Zauner <azet@azet.org>
2014-06-17 15:00:52 +0200

    lsbmajdistrelease deprecated for operatingsystemmajrelease

--- manifests/repos/redhat.pp
+++ manifests/repos/redhat.pp
@@ -1,7 +1,7 @@
 class omd::repos::redhat {
   #stable releases
   yumrepo { "omd":
-    baseurl => "https://labs.consol.de/repo/stable/rhel${::lsbmajdistrelease}/${::architecture}",
+    baseurl => "https://labs.consol.de/repo/stable/rhel${::operatingsystemmajrelease}/${::architecture}",
     descr => "Consol* Labs Repository",
     enabled => 1,
     gpgcheck => 1,
@@ -10,7 +10,7 @@ class omd::repos::redhat {
   
   #test releases
   yumrepo { "omd-testing":
-    baseurl => "https://labs.consol.de/repo/testing/rhel${::lsbmajdistrelease}/${::architecture}",
+    baseurl => "https://labs.consol.de/repo/testing/rhel${::operatingsystemmajrelease}/${::architecture}",
     descr => "Consol* Labs TESTING Repository",
     enabled => 1,
     gpgcheck => 1,
