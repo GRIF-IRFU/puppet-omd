503312567dfa5b5406559ba22d42a2a28908a935
fschaer <frederic.schaer@cea.fr>
2014-07-10 14:03:08 +0200

    fix PyYAML package for on debian

--- manifests/check_mk/plugins/puppet.pp
+++ manifests/check_mk/plugins/puppet.pp
@@ -1,15 +1,21 @@
 class omd::check_mk::plugins::puppet(
   $omd_sites=['all'],
 ) {
-  package { 'PyYAML': ensure => installed, }
+  $pyyaml_pkg=$::osfamily ? {
+    Debian => 'python-yaml',
+    default => PyYAML,
+  }
+
+  ensure_resource('package', $pyyaml_pkg, {} )
+  
   file { "/usr/lib/check_mk_agent/plugins/check_puppet.py":
     owner   => root,
     group   => root,
     ensure  => file,
     mode    => 0655,
     source  => "puppet:///modules/omd/plugins/puppet/check_puppet.py",
-    require => Package['PyYAML'],
-  } 
+    require => Package[$pyyaml_pkg],
+  }
   omd::check_mk::addtag{"puppet": omd_sites=>$omd_sites }
 }
 
