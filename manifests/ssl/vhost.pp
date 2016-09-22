66b574d9bcbecfb16b428519db6b5407e4213e88
fschaer <frederic.schaer@cea.fr>
2016-09-22 15:21:32 +0200

    use puppetlabs apache for SSL and do not re-define port 443 listen

--- manifests/ssl/vhost.pp
+++ manifests/ssl/vhost.pp
@@ -1,13 +1,13 @@
 /*
- * This is a class that you can use to create an apache VHOST, that will 
- * - listen on a defined port 
+ * This is a class that you can use to create an apache VHOST, that will
+ * - listen on a defined port
  * - activate and force SSL for every OMD site provided
  * - allow access to a secure and authenticate (restrict using certificates ) an HTTPS version of the unsecure http version, by using user certificates as the user names thanks to fakebasicauth
- * 
+ *
  * AND :
- * - for each 'site' given as mandatory argument, creates a < Location /> requiring the users created in the multisite htpasswd file 
+ * - for each 'site' given as mandatory argument, creates a < Location /> requiring the users created in the multisite htpasswd file
  */
- 
+
  class omd::ssl::vhost (
   $port=443,
   $sites, #this is a list of sites
@@ -16,12 +16,13 @@
   $ca_dir='/etc/grid-security/certificates',
   $sslciphersuite='ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS',
   $admin_mail='admin mail not set',
+  $priority='zzzz'
  )
  {
-   file { '/etc/httpd/conf.d/zzzz_omd_ssl.conf':
-    require => Package[httpd], #and debian ? apache2?
-    content => template('omd/ssl/vhost.erb'),
-    mode => '0644',
-    notify => Service[httpd],
-   }
+   include ::apache::ssl
+   ::apache::custom_config { 'omd_ssl':
+      ensure   => present,
+      content  => template('omd/ssl/vhost.erb'),
+      priority => $priority,
+    }
  }
